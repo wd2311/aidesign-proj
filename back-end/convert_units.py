c809139e0a55aca5a7812874f92a5271df728450
tigerygao <31675532+tigerygao@users.noreply.github.com>
2020-04-13 21:54:20 -0400

    Update convert_units.py

--- back-end/convert_units.py
+++ back-end/convert_units.py
@@ -1,4 +1,26 @@
 def conversion_table(given_unit, given_quantity):
+    if "cup" in given_unit or given_unit == "c.":
+        given_unit = "cup"
+    if "lb" in given_unit or "pound" in given_unit:
+        given_unit = "lb"
+    if "gram" == given_unit:
+        given_unit = "g"
+    if "teaspoon" in given_unit or given_unit == "tsp.":
+        given_unit = "tsp"
+    if "tablespoon" in given_unit:
+        given_unit = "tbsp"
+    if "ounce" in given_unit:
+        given_unit = "oz"
+    if "pint" in given_unit:
+        given_unit = "pt"
+    if "litre" == given_unit or "liter" == given_unit:
+        given_unit = "l"
+    if "quart" in given_unit:
+        given_unit = "qt"
+    if "gallon" in given_unit:
+        given_unit = "gal"
+
+    
     #### convert all weights to pounds
     if given_unit=='g':
         return 0.00220462*given_quantity
