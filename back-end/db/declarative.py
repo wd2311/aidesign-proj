944ad6367bc09e7d398aee7fd16b456334ea57d4
Shreya <shreya.chak15@gmail.com>
2020-04-25 18:44:26 -0400

    For new db Not working

--- back-end/db/declarative.py
+++ back-end/db/declarative.py
@@ -7,35 +7,47 @@ Base = declarative_base()
 
 class Recipe(Base):
     __tablename__ = 'recipe'
-    id = Column(Integer, primary_key=True)
+    recipe_id = Column(Integer, primary_key=True)
     ingredients = relationship('RecipeIngredient', back_populates='recipe')
-    name = Column(TEXT)
+    
+    recipe_name = Column(TEXT)
+    kcal = Column(Integer)
+    fat = Column(TEXT)
+    saturated_fat = Column(TEXT)
+    carbs = Column(TEXT)
+    sugars = Column(TEXT)
+    fibre = Column(TEXT)
+    protein = Column(TEXT)
+    salt = Column(TEXT)
     directions = Column(TEXT)
+    recipe_yield = Column(TEXT)
+    recipe_img = Column(TEXT)
+    
 
 class Ingredient(Base):
     __tablename__ = 'ingredient'
-    id = Column(Integer, primary_key=True)
+    ingr_id = Column(Integer, primary_key=True)
     recipes = relationship('RecipeIngredient', back_populates='ingredient')
     allergies = relationship('Allergen', back_populates='ingredient')
     name = Column(TEXT)
 
 class Allergy(Base):
     __tablename__ = 'allergy'
-    id = Column(Integer, primary_key=True)
+    allergy_id = Column(Integer, primary_key=True)
     ingredients = relationship('Allergen', back_populates='allergy')
     name = Column(TEXT)
 
 class Allergen(Base):
     __tablename__ = 'allergen'
-    allergy_id = Column(Integer, ForeignKey('allergy.id'), primary_key=True)
-    ingredient_id = Column(Integer, ForeignKey('ingredient.id'), primary_key=True)
+    allergy_id = Column(Integer, ForeignKey('allergy.allergy_id'), primary_key=True)
+    ingredient_id = Column(Integer, ForeignKey('ingredient.ingr_id'), primary_key=True)
     allergy = relationship('Allergy', back_populates='ingredients')
     ingredient = relationship('Ingredient', back_populates='allergies')
 
 class RecipeIngredient(Base):
     __tablename__ = 'recipe_ingredient'
-    recipe_id = Column(Integer, ForeignKey('recipe.id'), primary_key=True)
-    ingredient_id = Column(Integer, ForeignKey('ingredient.id'), primary_key=True)
+    recipe_id = Column(Integer, ForeignKey('recipe.recipe_id'), primary_key=True)
+    ingredient_id = Column(Integer, ForeignKey('ingredient.ingr_id'), primary_key=True)
     recipe = relationship('Recipe', back_populates='ingredients')
     ingredient = relationship('Ingredient', back_populates='recipes')
     quantity = Column(TEXT, nullable=True)
