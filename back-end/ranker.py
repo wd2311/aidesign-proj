558f356c76b816fc82195929cc6982cc4d61d274
Shreya <shreya.chak15@gmail.com>
2020-04-27 18:33:06 -0400

    Introduce random subsets

--- back-end/ranker.py
+++ back-end/ranker.py
@@ -4,7 +4,9 @@ import numpy as np
 from declarative import Base, Recipe, Ingredient, RecipeIngredient, Allergy, Allergen
 from sqlalchemy import create_engine, and_
 from sqlalchemy.orm import sessionmaker
-from sklearn.metrics.pairwise import cosine_similarity
+import random
+
+
 engine = create_engine('sqlite:///db/recipe.db')
 
 Base.metadata.bind = engine
@@ -17,29 +19,11 @@ session = DBSession()
 model = word2vec.Word2Vec.load("vectors/recipes.model")
 indexer = AnnoyIndexer(model, 2)
 
-top_50 = model.wv.most_similar('1', topn=20, indexer=indexer)
+top_50 = model.wv.most_similar('1', topn=50, indexer=indexer)
 
-def ranking(top_50, number):
+def ranking(top_50, num_of_samples, recipes_returned):
     ids = [id[0] for id in top_50]
-    candidates = []
-
-    for each_recipe in ids:
-        [recipe_entry] = session.query(Recipe).filter_by(recipe_id= int(each_recipe)).all()
-        # print(recipe_entry)
-        candidate_nutrition = {
-            'kcal': float(recipe_entry.kcal),
-            'fat': float(recipe_entry.fat[:-1]),
-            'saturates': float(recipe_entry.saturated_fat[:-1]),
-            'carbs': float(recipe_entry.carbs[:-1]),
-            'sugars': float(recipe_entry.sugars[:-1]),
-            'fibre': float(recipe_entry.fibre[:-1]),
-            'protein': float(recipe_entry.protein[:-1]),
-            'salt': float(recipe_entry.salt[:-1])
-        }
-        candidates.append(list(candidate_nutrition.values()))
-
-    candidates = np.array(candidates)
-    n, features = candidates.shape
+    subset_chosen = {}
 
     def iou(a, b):
         intersection = np.sum(np.dot(a, b))
@@ -48,26 +32,48 @@ def ranking(top_50, number):
             return 0
         return intersection / union
 
-    nutrition_similarity = {}
-    nutrition_diversity = {}
+    for i in range(num_of_samples):
+        ids_selected = random.sample(ids, recipes_returned) ### randomly select a subset from CG candidates
 
-    #### get pairwise similarity between every recipe
-    #### average dissimilarity for each recipe
-    #### choose recipe with max avg dissimilarity
+        candidates = []
+        for each_recipe in ids_selected:
+            [recipe_entry] = session.query(Recipe).filter_by(recipe_id= int(each_recipe)).all()
+            # print(recipe_entry)
+            candidate_nutrition = {
+                'kcal': float(recipe_entry.kcal),
+                'fat': float(recipe_entry.fat[:-1]),
+                'saturates': float(recipe_entry.saturated_fat[:-1]),
+                'carbs': float(recipe_entry.carbs[:-1]),
+                'sugars': float(recipe_entry.sugars[:-1]),
+                'fibre': float(recipe_entry.fibre[:-1]),
+                'protein': float(recipe_entry.protein[:-1]),
+                'salt': float(recipe_entry.salt[:-1])
+            }
+            candidates.append(list(candidate_nutrition.values()))
 
-    for i in range(n):
-        nutrition_similarity[i] = []
-        for j in range(n):
-            if j!=i:
-                val = iou(candidates[i, :], candidates[j, :])
-                nutrition_similarity[i].append(val)
-        nutrition_diversity[i] = -1 *(sum(nutrition_similarity[i])/len(nutrition_similarity[i]))
+        candidates = np.array(candidates)
+        n, features = candidates.shape
 
-    nutrition_diversity = sorted(nutrition_diversity.items(), key=lambda item: item[1], reverse=True)[:number]
+        #### get pairwise similarity between every recipe
+        #### average dissimilarity for each subset
+        #### choose subset with max avg dissimilarity
+        pairs = 0
+        nutrition_similarity = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                if j!=i:
+                    nutrition_similarity += iou(candidates[i, :], candidates[j, :])
+                    pairs +=1
 
-    ranker_ids = [id[0] for id in nutrition_diversity]
+        avg_sim = nutrition_similarity/pairs
+        subset_dissimilarity = -1 * avg_sim
+        subset_chosen[tuple(ids_selected)] = subset_dissimilarity
+
+    nutrition_diversity = sorted(subset_chosen.items(), key=lambda item: item[1], reverse=True) ####
+
+    ranker_ids = list(nutrition_diversity[0][0])
 
     return ranker_ids
-
-# ri = ranking(top_50, 10)
+#
+# ri = ranking(top_50, 8, 20)
 # print(ri)
